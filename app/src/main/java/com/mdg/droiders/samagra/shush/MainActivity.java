9a05b1eb182b413b684abcb1b09ee0e237e5bcab
Samagra <samagra14@gmail.com>
2017-07-20 03:04:25 +0530

    Adding Main Activity and a basic UI structure

--- app/src/main/java/com/mdg/droiders/samagra/shush/MainActivity.java
+++ app/src/main/java/com/mdg/droiders/samagra/shush/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
