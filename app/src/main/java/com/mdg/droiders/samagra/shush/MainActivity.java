abb58a5c70ab8c071109a6313f0331a62c281350
Samagra <samagra14@gmail.com>
2017-07-31 23:48:21 +0530

    API key changes and modifications to the listener

--- app/src/main/java/com/mdg/droiders/samagra/shush/MainActivity.java
+++ app/src/main/java/com/mdg/droiders/samagra/shush/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mdg.droiders.samagra.shush;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -13,13 +15,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.mdg.droiders.samagra.shush.data.PlacesContract;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
@@ -28,9 +37,11 @@ GoogleApiClient.OnConnectionFailedListener,
     private static final String LOG_TAG = "MainActivity";
 
     private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 111;
+    private static final int PLACE_PICKER_REQUEST = 1;
 
     private PlaceListAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private Button addPlaceButton;
 
     /**
      * Called when the activity is starting.
@@ -45,6 +56,14 @@ GoogleApiClient.OnConnectionFailedListener,
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mAdapter = new PlaceListAdapter(this);
         mRecyclerView.setAdapter(mAdapter);
+        addPlaceButton = (Button) findViewById(R.id.add_location_button);
+
+        addPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onAddPlaceButtonClicked();
+            }
+        });
 
         GoogleApiClient client = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -55,9 +74,53 @@ GoogleApiClient.OnConnectionFailedListener,
                 .build();
     }
 
+    /**
+     * Button click event handler for the add place button.
+     */
+    private void onAddPlaceButtonClicked() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, getString(R.string.need_location_permission_message), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Toast.makeText(this, getString(R.string.location_permissions_granted_message), Toast.LENGTH_SHORT).show();
+
+        try {
+            PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+            Intent placePickerIntent = builder.build(this);
+            startActivityForResult(placePickerIntent,PLACE_PICKER_REQUEST);
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode==PLACE_PICKER_REQUEST&&resultCode==RESULT_OK){
+            Place place = PlacePicker.getPlace(this,data);
+            if (place==null){
+                Log.i(LOG_TAG,"No place selected");
+                return;
+            }
+            String placeName = place.getName().toString();
+            String placeAddress = place.getAddress().toString();
+            String placeId = place.getId();
+
+            ContentValues values = new ContentValues();
+            values.put(PlacesContract.PlaceEntry.COLUMN_PLACE_ID,placeId);
+            getContentResolver().insert(PlacesContract.PlaceEntry.CONTENT_URI,values);
+        }
+    }
+
+
     @Override
     protected void onResume() {
         super.onResume();
+        //initialise location permissions checkbox
         CheckBox locationPermissionsCheckBox = (CheckBox) findViewById(R.id.location_permission_checkbox);
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
             locationPermissionsCheckBox.setChecked(false);
